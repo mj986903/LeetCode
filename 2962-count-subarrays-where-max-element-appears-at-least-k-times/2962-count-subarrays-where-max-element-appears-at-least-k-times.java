a0c1f716678a54c5b0c61fcd3055ad5d9db58a68
Mohit Joshi <122555503+mj986903@users.noreply.github.com>
2024-03-29 11:58:09 +0530

    Time: 5 ms (84.18%), Space: 61.3 MB (77.55%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,14 +1,8 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        int maxElement = nums[0];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(nums[0],1);
-        
-        for(int i=1;i<nums.length;i++){
-            if(maxElement < nums[i]){
-                maxElement = nums[i];
-            }
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        int maxElement = Integer.MIN_VALUE;
+        for(int n : nums){
+            maxElement = Math.max(maxElement,n);
         }
         
         int left = 0;
@@ -16,24 +10,19 @@ class Solution {
         int count = 0;
         long ans = 0;
         
-        while(right < nums.length){
+        while(right < nums.length || left > right){
             if(nums[right] == maxElement){
                 count++;
-                if(count >= k && count <= map.get(maxElement)){
-                    int temp = count;
-                    while(temp >= k){
-                        if(nums[left] == maxElement){
-                            temp--;
-                        }
-                        ans = ans + nums.length - right;
-                        left++;
-                    }
-                    count = temp;
+            }
+            while(count >= k){
+                if(nums[left] == maxElement){
+                    count--;
                 }
+                ans = ans + nums.length - right;
+                left++;
             }
             right++;
         }
-        
         return ans;
     }
 }
