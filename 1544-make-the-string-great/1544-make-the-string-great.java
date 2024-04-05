17842e03c834c9d8cb88b92b6582e9a1486da6b2
Mohit Joshi <122555503+mj986903@users.noreply.github.com>
2024-04-05 11:57:11 +0530

    Time: 4 ms (27.19%), Space: 42 MB (67.44%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -2,7 +2,11 @@ class Solution {
     public String makeGood(String s) {
         Stack<Character> st = new Stack();
         for(char c : s.toCharArray()){
-            if(!st.isEmpty() && Math.abs(c - st.peek()) == 32){
+            if(st.isEmpty()){
+                st.push(c);
+                continue;
+            }
+            if((Character.isUpperCase(c) && Character.isLowerCase(st.peek()) && Character.toUpperCase(st.peek()) == c) || Character.isUpperCase(st.peek()) && Character.isLowerCase(c) && Character.toUpperCase(c) == st.peek()){
                 st.pop();
             }else{
                 st.push(c);
