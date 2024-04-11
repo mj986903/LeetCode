e3d7c97727640d1ed950c26c7d2dd89a1c18a29e
Mohit Joshi <122555503+mj986903@users.noreply.github.com>
2024-04-11 14:08:58 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (89.50%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++){
+            res = res << 1;
+            res = res | (n & 1);
+            n = n >> 1;
+        }
+        return res;
+    }
+}
